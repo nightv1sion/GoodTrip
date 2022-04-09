687080f3e898d678f6cda9635bdd37a5fb0bcec4
nightv1sion <mr.uprivanov@yandex.ru>
2022-04-09 20:15:17 +0300

    delete

--- goodTrip/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace goodTrip.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
