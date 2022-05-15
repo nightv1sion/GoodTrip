1f561006108f35beaf3a1dfbeb4347b7d6301539
Saimon <komissarovsemen@mail.ru>
2022-05-16 01:32:48 +0300

    SearchPage

---
+++ goodtrip/Controllers/TourController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace goodtrip.Controllers
+{
+    public class TourController : Controller
+    {
+        [Route("{id}")]
+        public IActionResult Index(Guid id)
+        {
+            return View();
+        }
+    }
+}
