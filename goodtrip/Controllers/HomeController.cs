d7f14c4fc8621877cf98b20ecbb962b21237de47
nightv1sion <mr.uprivanov@yandex.ru>
2022-05-19 00:28:25 +0300

    FooterDescription

--- goodtrip/Controllers/HomeController.cs
+++ goodtrip/Controllers/HomeController.cs
@@ -33,11 +33,5 @@ namespace goodtrip.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public IActionResult AboutCont()
-        {
-
-            return View();
-        }
     }
 }
