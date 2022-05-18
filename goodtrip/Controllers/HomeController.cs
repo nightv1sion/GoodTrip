451d809c1d71c4d4d1313551cee71718cc562e93
Saimon <komissarovsemen@mail.ru>
2022-05-18 20:53:55 +0300

    PopDirReadyFixed

--- goodtrip/Controllers/HomeController.cs
+++ goodtrip/Controllers/HomeController.cs
@@ -33,7 +33,6 @@ namespace goodtrip.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
         
     }
 }
